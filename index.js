b84e58044c46ca35e1f13feacf9d0f119bf51087
kscript <1258@live.cn>
2018-12-28 14:02:59 +0800

    拆分内容

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
+var path = require('path');
 var merge = require('utils-merge');
-var pathFn = require('path');
+var _locals;
+var pathinfo;
 
 var config = hexo.config.wordCloud = merge({
-  output: 'wordCloud.html',
-  template: ''
+  output: 'tags/index.html',
+  title: '标签云',
+  script: '',
+  style: '',
+  template: '',
+  echarts: '',
+  main: '',
+  width: 1000,
+  height: 520,
 }, hexo.config.wordCloud);
 
-if (!config.path){
-  config.output = 'wordCloud.html';
+if (config.output){
+  pathinfo = path.parse(config.output);
+  config.dir = pathinfo.dir;
+  config.base = pathinfo.base;
+  config.tagpath = hexo.config.url + hexo.config.root + pathinfo.dir + '/';
+  hexo.extend.generator.register('wordCloud', function(locals){
+    _locals = locals;
+  });
+  hexo.extend.filter.register('before_exit', function(){
+    require('./lib/word-cloud').call(this, _locals);
+  });
 }
-
-if (!pathFn.extname(config.output)){
-  config.output += '.html';
-}
-if (config.template && !pathFn.extname(config.template)){
-  config.template += '.html';
-}
-
-hexo.extend.generator.register('wordCloud', require('./lib/word-cloud'));
+// [
+//   'before_post_render',
+//   'after_post_render',
+//   'before_exit',
+//   'before_generate',
+//   'after_generate',
+//   'template_locals',
+//   'after_init',
+//   'new_post_path',
+//   'post_permalink',
+//   'after_render',
+//   'server_middleware'
+// ].forEach(element => {
+//   hexo.extend.filter.register(element, function(){
+//     console.log(element)
+//   });
+// });
---
+++ lib/element.swig
@@ -0,0 +1,3 @@
+<div class="word-cloud">
+  <div id="main" style="width: <!-- width -->px;height: <!-- height -->px;margin: 0 auto;"></div>
+</div>
